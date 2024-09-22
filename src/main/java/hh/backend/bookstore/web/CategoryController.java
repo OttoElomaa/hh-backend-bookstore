8f4a52c8788a52842824ba52bbc30e4f2e080aa6
OttoElomaa <otto.elomaa99@gmail.com>
2024-09-22 17:50:07 +0300

    Category List Page with 'Add Category' Option Added

--- src/main/java/hh/backend/bookstore/web/CategoryController.java
+++ src/main/java/hh/backend/bookstore/web/CategoryController.java
@@ -3,8 +3,11 @@ package hh.backend.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hh.backend.bookstore.domain.Book;
+import hh.backend.bookstore.domain.Category;
 import hh.backend.bookstore.domain.CategoryRepository;
 
 
@@ -20,4 +23,20 @@ public class CategoryController {
         model.addAttribute("categories", repository.findAll());
         return "categorylist";
     }
+
+
+	// ADD
+
+	@RequestMapping(value = "/addcat")
+    public String addCategory(Model model){
+        model.addAttribute("newCategory", new Category());
+        return "addcategory";
+    }
+
+	@PostMapping(value = "/savenewcat")
+    public String saveNew(Category newCat){
+        repository.save(newCat);
+        return "redirect:/categorylist";
+    }
+ 
 }
